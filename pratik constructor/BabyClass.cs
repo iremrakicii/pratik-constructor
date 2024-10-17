68a4c0fa85fc36b5e1d000e8733b3aea333de1d6
İrem Rakıcı <iremrakici53@hotmail.com>
2024-10-17 14:38:35 +0300

    Proje dosyası ekle.

---
+++ pratik constructor/BabyClass.cs
@@ -0,0 +1,62 @@
+﻿namespace pratik_constructor
+{
+    public class BabyClass
+    {
+        private string _babyName;
+        private string _babySurname;
+        private DateTime _babyDate;
+
+        public BabyClass()
+        {
+            _babyDate = DateTime.Now;
+            Console.WriteLine($"Ingaaaa ! , Doğduğu gün ve saat: {_babyDate}");
+        }
+        public BabyClass(string babyName, string babySurname)
+        {
+            _babyName = babyName;
+            _babySurname = babySurname;
+            _babyDate = DateTime.Now;
+            Console.WriteLine($"Ingaaaa ! , Doğduğu gün ve saat: {_babyDate}");
+        }
+
+        public string AddName
+        {
+            get
+            {
+                return _babyName;
+            }
+
+            set
+            {
+                _babyName = value;
+            }
+        }
+        public string AddSurname
+        {
+            get
+            {
+                return _babySurname;
+            }
+            set
+            {
+                _babySurname = value;
+            }
+
+        }
+        public DateTime AddBirthDate
+        {
+            get
+            {
+                return _babyDate;
+            }
+
+            set
+            {
+                _babyDate = value;
+            }
+        }
+
+    }
+
+
+}
---
+++ pratik constructor/Program.cs
@@ -0,0 +1,18 @@
+﻿using pratik_constructor;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        //// Parametreli constructor ile nesne oluşturma
+        BabyClass bebek = new BabyClass("Ayşe", "Öztürk");
+        Console.WriteLine($"Bebeğin adı : {bebek.AddName}, Bebeğin soyadı: {bebek.AddSurname}");
+        Console.WriteLine("--------------------------------------");
+        // // Default constructor ile nesne oluşturma
+        BabyClass bebek1 = new BabyClass();
+        bebek1.AddName = "Ahmet";
+        bebek1.AddSurname = "Ertürk";
+        Console.WriteLine($"Bebeğin adı: {bebek1.AddName}, Bebeğin soyadı: {bebek1.AddSurname}");
+
+    }
+}
